2b103d23a740ef867ad5352566ba0ec24e86e95a
danielbonifacio <danielbonifacio@outlook.com>
2019-03-14 12:52:20 -0300

    whoAmI middleware

---
+++ src/middleware/whoAmI.js
@@ -0,0 +1,29 @@
+const { helpers } = require('@controller/user');
+const error = require('@service/error');
+/**
+ * Adds user info in the request object
+ * @param {object} req express request object
+ * @param {object} res express response object
+ */
+const whoAmI = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return error({
+      res,
+      status: 400,
+      payload: 'Token não providenciado.',
+    });
+  }
+
+  try {
+    const userId = await helpers.decodeToken(authorization);
+    const user = await helpers.getUserInfo(userId);
+    req.me = user;
+    next();
+  } catch (err) {
+    error({ res, log: err });
+  }
+};
+
+module.exports = whoAmI;
