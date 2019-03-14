5d5be09b5d9d0e6c9c89a45779848883e87493b4
danielbonifacio <danielbonifacio@outlook.com>
2019-03-14 18:30:20 -0300

    fix profile schema variable name

--- src/model/profile.js
+++ src/model/profile.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({
+const ProfileSchema = new Schema({
   name: {
     type: String,
     required: true,
@@ -20,4 +20,4 @@ const UserSchema = new Schema({
   },
 });
 
-module.exports = mongoose.model('profile', UserSchema);
+module.exports = mongoose.model('profile', ProfileSchema);
